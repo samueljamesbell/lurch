9963e832229402a0c0d165fe2a6c770b7ac2211b
Sam Bell <samueljamesbell@gmail.com>
2013-04-17 23:15:23 +0100

    Now creating or finding rules based on inheriting Handler

--- lib/lurch/handler.rb
+++ lib/lurch/handler.rb
@@ -9,7 +9,14 @@ module Lurch
       attr_accessor :rules
     end
 
+    def self.inherited(subclass)
+      subclass.define_singleton_method(:rule) do |pattern, &block|
+        self.register_rule(subclass.name.split('::')[-1], pattern, &block)
+      end
+    end
+
     def self.match(event)
+      puts Handler.rules.inspect
       Handler.rules.sort_by { |r| r.priority }.each do |rule|
         # match each
         # params = matched chunks
@@ -18,10 +25,10 @@ module Lurch
       end
     end
 
-    protected
+    def self.register_rule(handler, pattern, &block)
+      rule = Rule[:pattern => pattern.to_s, :handler => handler] ||
+             Rule.create(:pattern => pattern.to_s, :handler => handler)
 
-    def self.rule(pattern, &block)
-      rule = Rule[pattern, self.class] or Rule.create(:pattern => pattern, :handler => self.class)
       rule.block = block
       Handler.rules << rule
     end
