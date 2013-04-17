97e0f53e9fc88f41d60e6647cda129c3ec67ee97
Sam Bell <samueljamesbell@gmail.com>
2013-04-17 16:07:33 +0100

    Crude implementation of Handler

---
+++ lib/lurch/handler.rb
@@ -0,0 +1,28 @@
+module Lurch
+  class Handler
+
+    @rules = []
+
+    class << self
+      attr_accessor :rules
+    end
+
+    def self.match(event)
+      @rules.sort_by { |r| r.priority }.each do |rule|
+        # match each
+        # params = matched chunks
+        # rule.handler.new.instance_eval(rule.block)
+        # if success, update frecency
+      end
+    end
+
+    protected
+
+    def self.rule(pattern, &block)
+      rule = Rule[pattern, self.class] or Rule.create(:pattern => pattern, :handler => self.class)
+      rule.block = block
+      Handler.rules << rule
+    end
+
+  end
+end
--- lib/lurch/rule.rb
+++ lib/lurch/rule.rb
@@ -1,7 +1,13 @@
 module Lurch
   class Rule < Sequel::Model
 
+    attr_accessor :block
+
     set_primary_key [:handler, :pattern]
+
+    def priority
+      frecency # unless coming from Lurch::Output::Handler?
+    end
 
     def frecency
       0 # calculate from last_updated and popularity
