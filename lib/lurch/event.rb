e88e3e6ee6ae0bf8f033f82120d4000c97fa2fa4
Sam Bell <samueljamesbell@gmail.com>
2013-04-17 12:30:43 +0100

    Added rough Event class

---
+++ lib/lurch/event.rb
@@ -0,0 +1,29 @@
+module Lurch
+  class Event
+
+    attr_accessor :service, :user, :message
+
+    def initialize(service, user, message, bypass = false)
+      @service = service
+      @user = user
+      @message = message
+      @bypass = bypass
+    end
+
+    def priority
+      if command?  2
+      elsif bypass?  1
+      else 0
+      end
+    end
+
+    def command?
+      @service == :command
+    end
+
+    def bypass?
+      @bypass
+    end
+
+  end
+end
