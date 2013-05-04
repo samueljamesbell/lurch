98085bd42652874f16128a7eb6ca406e4863d4ea
Sam Bell <samueljamesbell@gmail.com>
2013-05-04 19:52:37 +0100

    Prevent handled messages from being output

--- lib/lurch/handlers/output.rb
+++ lib/lurch/handlers/output.rb
@@ -3,7 +3,7 @@ module Lurch
     class Output < Lurch::Handler
 
       rule /.*/ do
-        if event.command?
+        if event.command? && ! event.handled?
           server.send(%Q{Sorry, I don't understand what you mean by "#{event.message}"\n})
         else
           server.send(event.message + "\n") unless event.silent? || event.handled?
