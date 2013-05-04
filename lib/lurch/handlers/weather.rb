6e29f439a9c2ba4e4afdf401903c2500d46ab57e
Sam Bell <samueljamesbell@gmail.com>
2013-05-04 19:52:45 +0100

    Working Weather handler

--- lib/lurch/handlers/weather.rb
+++ lib/lurch/handlers/weather.rb
@@ -1,10 +1,20 @@
+require 'net/http'
+require 'json'
+
 module Lurch
   module Handlers
     class Weather < Handler
 
-      rule /weather (?:in|at) (\w*)/ do
-        message 'The weather is nice today'
-        succeed
+      rule /weather (?:doing |like )?(?:in|at) (\w*)/ do
+        location_name = matches[1]
+
+        uri = URI("http://api.openweathermap.org/data/2.5/weather?q=#{location_name}&units=metric")
+        response = JSON.parse(Net::HTTP.get(uri))
+
+        description = response['weather'][0]['description'].capitalize
+        temperature = response['main']['temp']
+
+        message "#{description} and #{temperature} degrees"
       end
 
     end
