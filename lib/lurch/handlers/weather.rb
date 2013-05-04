d57c6a380df8fd10b052266491835156e61c47a7
Sam Bell <samueljamesbell@gmail.com>
2013-05-04 15:03:22 +0100

    Simplified weather handler

--- lib/lurch/handlers/weather.rb
+++ lib/lurch/handlers/weather.rb
@@ -2,7 +2,7 @@ module Lurch
   module Handlers
     class Weather < Handler
 
-      rule /weather\s(in|at)\s(\w*)/ do
+      rule /weather (in|at) (\w*)/ do
         message 'The weather is nice today'
       end
 
